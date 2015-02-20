edd85f95b20c065b4394db2b6d8cfb09043f6076
Rocky Randalls <rrandalls@gmail.com>
2015-02-19 20:24:07 -0500

    Add sessions new/log_in action and routes, pass first test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    staff = Staff.find_by_email(params[:email])
+    if staff && staff.authenticate(params[:password])
+      session[:staff_id] = staff.id
+      flash[:notice] = "Welcome!"
+      redirect_to bills_path
+    else
+      flash.now[:notice] = "Invalid e-mail or password."
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get 'log_in' => 'sessions#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
