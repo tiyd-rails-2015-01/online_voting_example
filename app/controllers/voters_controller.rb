978d5f69bc40f8d978116c7497a6b6dc2a409991
Ashley Ellis <anellis12@gmail.com>
2015-02-20 12:07:53 -0500

    Add new voter form

--- app/controllers/voters_controller.rb
+++ app/controllers/voters_controller.rb
@@ -1,10 +1,12 @@
 class VotersController < ApplicationController
+  before_action :set_voter, only: [:show, :edit, :update]
   before_action :logged_in?
-  
+
   def index
   end
 
   def new
+    @voter = Voter.new
   end
 
   def edit
@@ -14,11 +16,27 @@ class VotersController < ApplicationController
   end
 
   def create
+    @voter = Voter.new(voter_params)
+    if @voter.save
+      redirect_to bills_path, notice: "Voter Saved!"
+    else
+      redirect_to new_voter_path, notice: "All fields are required."
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def set_voter
+    @voter = Voter.find(params[:id])
+  end
+
+  def voter_params
+    params.require(:voter).permit(:email, :name, :password)
   end
 end
